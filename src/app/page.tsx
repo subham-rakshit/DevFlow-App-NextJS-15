abe8444c50ccfdcdab306747c1ad0a77ceaa956a
subham-rakshit <subhamrakshit667@gmail.com>
2024-11-02 16:18:04 +0530

    Font-setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 const Home = () => {
   return (
     <div>
-      <h1 className="text-4xl font-black text-light-500">
-        Welcome to the World of Next.js
+      <h1 className="h1-bold">Welcome to the World of Next.js</h1>
+      <h1 className="h2-semibold font-inter">My name is Subham Rakshit</h1>
+      <h1 className="h2-semibold font-space-grotesk">
+        My name is Subham Rakshit
       </h1>
     </div>
   );
