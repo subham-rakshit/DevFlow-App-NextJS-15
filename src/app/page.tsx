bea87fdbe25f56469609d69e09fcf2176df3fab2
subham-rakshit <subhamrakshit667@gmail.com>
2024-11-02 15:20:09 +0530

    ESLint & Prettier Setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
     <div>
-      <h1 className="text-4xl text-violet-700 font-black">
+      <h1 className="text-4xl font-black text-violet-700">
         Welcome to the World of Next.js
       </h1>
     </div>
